ff8ec1500683e0735b5189d66ba0b9c833022213
liucyi <liucyi@sina.cn>
2016-03-04 00:01:42 +0800

    修改样式

--- .idea/User.js
+++ .idea/User.js
@@ -1,3 +1,29 @@
 /**
  * Created by Administrator on 2016/3/3.
  */
+$(function(){
+
+  //var box= $("div").css(["color","width","height"]);
+
+
+ //$.each(box,function(attr,value){
+ // alert(attr+":"+value);
+ //})
+
+ //$("div").each(function(ind,ele){
+ // alert(ind+":"+ele);
+ //})
+ //$("div").css({
+ // "color":"red",
+ // "background":"green",
+ // "width":"200px"
+ //
+ //})
+ $("div").css("width",function(index,value){
+  return parseInt(value)-500+"px";
+ })
+ $("div").onmousedown(function(){
+   $(this).css("background","red");
+  alert(1);
+ })
+})
