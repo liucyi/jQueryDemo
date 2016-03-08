17c26e302e1a43df9c9175fc3120ac293e93d8b4
liucyi <liucyi@hotmail.com>
2016-03-08 15:28:00 +0800

    Update ajax.js

--- .idea/ajax.js
+++ .idea/ajax.js
@@ -1,12 +1,41 @@
-/**
- * Created by Administrator on 2016/3/6.
- */
-$(function(){
+; (function ($) {
+ 	//$.extend({//全局	 
+	//	"nav": function (color) {
+	//		$(".nav").css({
+	//			"list-style": "none",
+	//			"margin": 0,
+	//			"padding": 0,
+	//			"display": "none",
+	//			"color":color
 
-    //$("input").click(function(){
-    //    $("#box").load("index.html")
-    //})
-    $("input").click(function(){
-        $("#box").load("test.php")
-    })
-})
+	//		});
+	//		$(".nav").parent().hover(function () {
+	//			$(this).find(".nav").slideDown("slow");
+	//		}, function () {
+	//			$(this).find(".nav").stop().slideUp("slow");
+	//		})
+	//	}
+	//	})
+
+
+
+	//****************************************************
+
+		$.fn.extend({//局部
+			"nav": function (color) {
+				$(this).find(".nav").css({
+					"list-style": "none",
+					"margin": 0,
+					"padding": 0,
+					"display": "none",
+					"color": color
+
+				});
+				$(this).find(".nav").parent().hover(function () {
+					$(this).find(".nav").slideDown("slow");
+				}, function () {
+					$(this).find(".nav").stop().slideUp("slow");
+				})
+			}
+		})
+})(jQuery)
